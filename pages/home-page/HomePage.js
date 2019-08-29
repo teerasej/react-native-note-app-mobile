38c012d6c4c1ceb11f8632236015ca552a1ad8d1
Teerasej <teerasej@gmail.com>
2019-08-29 23:57:35 +0700

    สร้างหน้า HomePage และนำมาใช้งาน

---
+++ pages/home-page/HomePage.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+export class HomePage extends Component {
+    static propTypes = {
+       
+    }
+
+    render() {
+        return (
+            <View>
+                <Text> prop </Text>
+            </View>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    
+})
+
+const mapDispatchToProps = {
+    
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage)
