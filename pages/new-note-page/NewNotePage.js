924139095d7f48e6b67bd7b4a2a1f8f4fdcf2662
Teerasej <teerasej@gmail.com>
2019-08-30 16:18:57 +0700

    สร้างส่วนรับข้อมูลโดยใช้ redux-form

--- pages/new-note-page/NewNotePage.js
+++ pages/new-note-page/NewNotePage.js
@@ -3,12 +3,25 @@ import { View } from 'react-native'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Container, Header, Title, Content, List, ListItem, Text, Left, Right, Body, Button } from 'native-base';
-
+import { Field,reduxForm } from 'redux-form';
 
 export class NewNotePage extends Component {
     static propTypes = {
        
     }
+
+    renderInput({ input, label, type, meta: { touched, error, warning } }){
+        var hasError= false;
+        if(error !== undefined){
+          hasError= true;
+        }
+        return( 
+          <Item error= {hasError}>
+            <Input {...input}/>
+            {hasError ? <Text>{error}</Text> : <Text />}
+          </Item>
+        )
+      }
 
     render() {
         return (
@@ -19,6 +32,7 @@ export class NewNotePage extends Component {
                     </Body>
                 </Header>
                 <Content padder>
+                    <Field name="message" component={this.renderInput} />
                     <Button block primary >
                         <Text>Save</Text>
                     </Button>
