9aa4df623242136376def4d180627a18552dc91a
Teerasej <teerasej@gmail.com>
2019-08-30 17:11:11 +0700

    เพิ่ง form reducer เข้าไปใน store

--- redux/reducers/root.reducer.js
+++ redux/reducers/root.reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import noteReducer from './note.reducer';
+import { reducer as formReducer } from 'redux-form';
 
 export default () => combineReducers({
-  note: noteReducer
+  note: noteReducer,
+  form: formReducer
 }) 
