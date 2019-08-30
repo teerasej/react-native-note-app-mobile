a5be21c5d028f4cddd69d2f351649937a47bbee3
Teerasej <teerasej@gmail.com>
2019-08-30 22:58:53 +0700

    สร้าง root saga

--- redux/store.js
+++ redux/store.js
@@ -3,15 +3,23 @@ import { logger } from 'redux-logger';
 
 import createRootReducer from "./reducers/root.reducer";
 
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './sagas/root.saga';
+
+const sagaMiddleware = createSagaMiddleware()
+
 export default function configureStore() {
     const store = createStore(
         createRootReducer(),
         compose(
             applyMiddleware(
-                logger
+                logger,
+                sagaMiddleware
             ),
         ),
     );
+
+    sagaMiddleware.run(rootSaga);
 
     return store;
 }
