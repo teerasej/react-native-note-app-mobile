39f719b972a3f57ddbdf6b9d97c4302b57dc8ab3
Teerasej <teerasej@gmail.com>
2019-08-30 15:38:31 +0700

    ปรับตำแหน่งของ Header title ด้วย Body

--- pages/home-page/HomePage.js
+++ pages/home-page/HomePage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { View } from 'react-native'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Container, Header, Title, Content, List, ListItem, Text } from 'native-base';
+import { Container, Header, Title, Content, List, ListItem, Text, Left, Right, Body, } from 'native-base';
 
 export class HomePage extends Component {
     static propTypes = {
@@ -13,7 +13,9 @@ export class HomePage extends Component {
         return (
             <Container>
                 <Header>
+                    <Body>
                     <Title>Note</Title>
+                    </Body>
                 </Header>
 
                 <Content>
