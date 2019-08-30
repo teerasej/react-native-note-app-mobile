090a67fc2f407a04d71e077b7caab2a64712a0ef
Teerasej <teerasej@gmail.com>
2019-08-30 11:59:44 +0700

    import List และ ListItem

--- pages/home-page/HomePage.js
+++ pages/home-page/HomePage.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Container, Header, Title, Content } from 'native-base';
+import { Container, Header, Title, Content, List, ListItem } from 'native-base';
 
 export class HomePage extends Component {
     static propTypes = {
-       
+        
     }
 
     render() {
@@ -15,21 +15,32 @@ export class HomePage extends Component {
                 <Header>
                     <Title>Note</Title>
                 </Header>
-            
+
                 <Content>
+                    <List>
+                        <ListItem>
+                            <Text>Simon Mignolet</Text>
+                        </ListItem>
+                        <ListItem>
+                            <Text>Nathaniel Clyne</Text>
+                        </ListItem>
+                        <ListItem>
+                            <Text>Dejan Lovren</Text>
+                        </ListItem>
+                    </List>
                 </Content>
             </Container>
-            
+
         )
     }
 }
 
 const mapStateToProps = (state) => ({
-    
+
 })
 
 const mapDispatchToProps = {
-    
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage)
