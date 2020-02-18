39dd488b19972801b9e96d92358e21fb1f93c279
Teerasej <teerasej@gmail.com>
2020-02-19 00:16:02 +0700

    move logic to action instead

--- redux/note.reducer.js
+++ redux/note.reducer.js
@@ -1,6 +1,4 @@
 import actions from "./actions"
-import AsyncStorage from '@react-native-community/async-storage';
-
 const initialState = {
 
     notes: [
@@ -10,27 +8,12 @@ const initialState = {
     ]
 }
 
-const updateStorage = async (message) => {
-    try {
-        const messageArrayString = await AsyncStorage.getItem('@messageArray')
-        if (messageArrayString !== null) {
-            let messageArray:Array = JSON.parse(messageArrayString);
-            messageArray.push(message);
-            let updatedMessageArrayString = JSON.stringify(messageArray);
-            await AsyncStorage.setItem('@messageArray', updatedMessageArrayString);
-            console.log('updated complete');
-        }
-        
-    } catch (e) {
-        console.error(e);
-    }
-}
+
 
 export default (state = initialState, { type, payload }) => {
     switch (type) {
 
         case actions.Types.SAVE_NEW_NOTE:
-            updateStorage(payload);
             return { ...state, notes: [...state.notes, { title: payload }] }
 
         default:
