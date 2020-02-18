d582c11b2e26d73db6d99923afe45332b7452b9a
Teerasej <teerasej@gmail.com>
2020-02-18 23:24:58 +0700

    รับ action และอัพเดต state

--- redux/note.reducer.js
+++ redux/note.reducer.js
@@ -1,12 +1,19 @@
+import actions from "./actions"
+
 const initialState = {
 
+    notes: [
+        { title: 'a' },
+        { title: 'b' },
+        { title: 'c' }
+    ]
 }
 
 export default (state = initialState, { type, payload }) => {
     switch (type) {
 
-    case '':
-        return { ...state, ...payload }
+    case actions.Types.SAVE_NEW_NOTE:
+        return { ...state, notes: [...state.notes, { title: payload }] }
 
     default:
         return state
