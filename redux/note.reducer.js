ca36187e896eb52d665b8a90cca896b30ece59a5
Teerasej <teerasej@gmail.com>
2020-02-18 23:52:21 +0700

    เพิ่มข้อมูลใน storage ก่อนส่งให้ state

--- redux/note.reducer.js
+++ redux/note.reducer.js
@@ -1,4 +1,5 @@
 import actions from "./actions"
+import AsyncStorage from '@react-native-community/async-storage';
 
 const initialState = {
 
@@ -9,13 +10,32 @@ const initialState = {
     ]
 }
 
+const updateStorage = async (message) => {
+    try {
+        const messageArrayString = await AsyncStorage.getItem('@messageArray')
+        if (messageArrayString !== null) {
+            let messageArray:Array = JSON.parse(messageArrayString);
+            messageArray.push(message);
+            let updatedMessageArrayString = JSON.stringify(messageArray);
+            await AsyncStorage.setItem('@messageArray', updatedMessageArrayString);
+            console.log('updated complete');
+        }
+        
+    } catch (e) {
+        console.error(e);
+    }
+}
+
 export default (state = initialState, { type, payload }) => {
     switch (type) {
 
-    case actions.Types.SAVE_NEW_NOTE:
-        return { ...state, notes: [...state.notes, { title: payload }] }
+        case actions.Types.SAVE_NEW_NOTE:
+            updateStorage(payload);
+            return { ...state, notes: [...state.notes, { title: payload }] }
 
-    default:
-        return state
+        default:
+            return state
     }
 }
+
+
