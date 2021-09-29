0c7c914951db51fb2313c34e0b7bccbec6fde98d
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-09-30 01:12:39 +0700

    load font with useFont hook

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import 'react-native-gesture-handler';
 import AppLoading from 'expo-app-loading';
 import { Container, Text } from 'native-base';
-import * as Font from 'expo-font';
+import { useFonts } from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import HomePage from './pages/home-page/HomePage';
 import NewNotePage from './pages/new-note-page/NewNotePage';
@@ -12,27 +12,13 @@ import NewNotePage from './pages/new-note-page/NewNotePage';
 
 export default function App() {
 
-  const [isReady, setIsReady] = useState(false)
+  let [fontsLoaded] = useFonts({
+    Roboto: require('native-base/Fonts/Roboto.ttf'),
+    Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+    ...Ionicons.font,
+  });
 
-  useEffect(() => {
-
-    // สั่งให้ Load font เพื่อใช้งานใน UI Component ที่สร้างด้วย Native base
-    let loadFont = async () => {
-      await Font.loadAsync({
-        Roboto: require('native-base/Fonts/Roboto.ttf'),
-        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-        ...Ionicons.font,
-      });
-
-      // ตั้งค่า State ใหม่ เพื่อให้ App component ทำการ render ตัวเองอีกครั้ง
-      setIsReady(true)
-    }
-
-    // เริ่มการ load font
-    loadFont();
-  }, [])
-
-  if (!isReady) {
+  if (!fontsLoaded) {
     return (
       <AppLoading />
     )
