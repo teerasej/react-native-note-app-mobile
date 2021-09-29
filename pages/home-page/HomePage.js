87209b952526a2a6ab3cef777a7b1c3d365005c3
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-09-30 00:02:08 +0700

    update to function component

--- pages/home-page/HomePage.js
+++ pages/home-page/HomePage.js
@@ -3,32 +3,28 @@ import { View } from 'react-native'
 import { connect } from 'react-redux'
 import { Container, Header, Title, Content, List, ListItem, Text, Left, Right, Body, Button, Icon } from 'native-base';
 
-export class HomePage extends Component {
 
+export default function HomePage() {
 
-    notes = [
+    let notes = [
         { title: 'a' },
         { title: 'b' },
         { title: 'c' }
     ]
 
-    render() {
-        return (
-
-            <Content>
-                <List>
-                    <ListItem>
-                        <Text>A</Text>
-                    </ListItem>
-                    <ListItem>
-                        <Text>B</Text>
-                    </ListItem>
-                </List>
-            </Content>
-
-        )
-    }
+    return (
+        <Content>
+            <List>
+                <ListItem>
+                    <Text>A</Text>
+                </ListItem>
+                <ListItem>
+                    <Text>B</Text>
+                </ListItem>
+            </List>
+        </Content>
+    )
 }
 
 
-export default HomePage
+
